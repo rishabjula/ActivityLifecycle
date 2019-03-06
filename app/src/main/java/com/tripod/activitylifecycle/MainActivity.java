3738029d264aa5b508cd9a2cf1102c436eaa3c25
Rishab Jula <rishab.jula@gmail.com>
2019-03-06 12:05:10 +0530

    Initial commit

---
+++ app/src/main/java/com/tripod/activitylifecycle/MainActivity.java
@@ -0,0 +1,48 @@
+package com.tripod.activitylifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this,"OnCreate called",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(MainActivity.this,"OnPause called",Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Toast.makeText(MainActivity.this,"OnPostResume called",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(MainActivity.this,"OnStop called",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(MainActivity.this,"OnDestroy called",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+   protected void onStart()
+    {
+        super.onStart();
+        Toast.makeText(MainActivity.this,"OnStart called",Toast.LENGTH_LONG).show();
+    }
+}
